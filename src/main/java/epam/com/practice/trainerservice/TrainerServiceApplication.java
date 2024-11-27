42029a8175a8121541c4169b442b4d7304911f09
Dana <Dana.ackerman0914@gmail.com>
2024-11-27 13:05:00 +0500

    Add main class

---
+++ src/main/java/epam/com/practice/trainerservice/TrainerServiceApplication.java
@@ -0,0 +1,13 @@
+package epam.com.practice.trainerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerServiceApplication.class, args);
+    }
+
+}
