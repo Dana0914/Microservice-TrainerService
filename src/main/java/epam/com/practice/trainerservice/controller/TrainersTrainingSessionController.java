086305cbb70c63feb1028b5d6a466ff2065bf308
Dana <Dana.ackerman0914@gmail.com>
2024-12-04 16:57:09 +0500

    Add swagger doc

--- src/main/java/epam/com/practice/trainerservice/controller/TrainersTrainingSessionController.java
+++ src/main/java/epam/com/practice/trainerservice/controller/TrainersTrainingSessionController.java
@@ -1,14 +1,26 @@
 package epam.com.practice.trainerservice.controller;
 
 
+import epam.com.practice.trainerservice.config.SwaggerConfiguration;
 import epam.com.practice.trainerservice.dto.TrainersTrainingWorkloadDTO;
 import epam.com.practice.trainerservice.service.TrainerWorkloadService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @RestController
 @RequestMapping(value = "api/workload")
+@Api(tags = {SwaggerConfiguration.TRAINER_WORKLOAD_TAG})
+@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class TrainersTrainingSessionController {
 
     private final TrainerWorkloadService trainerWorkloadService;
@@ -17,6 +29,17 @@ public class TrainersTrainingSessionController {
         this.trainerWorkloadService = trainerWorkloadService;
     }
 
+    @Operation(summary = "Update training session")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated the training session",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TrainersTrainingWorkloadDTO.class))}),
+            @ApiResponse(responseCode = "201", description = "Training session created",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = TrainersTrainingWorkloadDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Forbidden - You don't have permission", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Training not found", content = @Content)
+    })
     @PostMapping(value = "/trainers/trainings")
     public ResponseEntity<String> updateTrainersTrainingWorkload(
             @RequestBody TrainersTrainingWorkloadDTO trainersTrainingWorkloadRequest) {
@@ -26,16 +49,5 @@ public class TrainersTrainingSessionController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Training workload created successfully");
     }
-
-
-
-    @GetMapping(value = "/trainers/trainings/monthly-summary")
-    public ResponseEntity<Void> getTrainingMonthlySummary() {
-        trainerWorkloadService.getTrainingMonthlySummary();
-        return ResponseEntity.status(HttpStatus.OK).build();
-
-    }
-
-
 
 }
