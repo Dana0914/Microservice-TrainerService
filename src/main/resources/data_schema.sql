4a6bbff49fe2fe3855c9b53be803d36a2e9b1fc6
Dana <Dana.ackerman0914@gmail.com>
2024-12-04 16:49:17 +0500

    Add in memory db schema - H2

--- src/main/resources/data_schema.sql
+++ src/main/resources/data_schema.sql
@@ -1,18 +1,35 @@
 CREATE TABLE trainer (
-    id BIGINT PRIMARY KEY NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     username VARCHAR(55) UNIQUE NOT NULL,
     firstname VARCHAR(55) NOT NULL,
     lastname VARCHAR(55) NOT NULL,
-    status ENUM('ACTIVE', 'INACTIVE') CHECK (status IN ('ACTIVE', 'INACTIVE'))
+    is_active BOOLEAN DEFAULT FALSE
 
 );
 
 CREATE TABLE training (
-    id BIGINT PRIMARY KEY NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     trainer_id INT8 NOT NULL,
-    training_date TIMESTAMP DEFAULT CURRENT_DATE,
+    training_date DATE DEFAULT CURRENT_DATE,
     training_duration INT4 NOT NULL,
     action_type ENUM('ADD', 'DELETE') CHECK (action_type in ('ADD', 'DELETE')),
     FOREIGN KEY (trainer_id) REFERENCES trainer(id)
 
 );
+
+CREATE TABLE trainer_workload (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    trainer_id INT8 NOT NULL,
+    years INTEGER NOT NULL,
+    months INTEGER NOT NULL,
+    training_hours INTEGER NOT NULL,
+    FOREIGN KEY (trainer_id) REFERENCES trainer(id)
+);
+
+
+
+
+
+
+
+
