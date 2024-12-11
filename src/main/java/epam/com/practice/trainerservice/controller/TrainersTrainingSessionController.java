bdc5c9b304aeb88bbddb3883047e53781f68d3da
Dana <Dana.ackerman0914@gmail.com>
2024-12-11 23:06:17 +0500

    Feature: Add monthly summary method

--- src/main/java/epam/com/practice/trainerservice/controller/TrainersTrainingSessionController.java
+++ src/main/java/epam/com/practice/trainerservice/controller/TrainersTrainingSessionController.java
@@ -1,26 +1,21 @@
 package epam.com.practice.trainerservice.controller;
 
-
-import epam.com.practice.trainerservice.config.SwaggerConfiguration;
 import epam.com.practice.trainerservice.dto.TrainersTrainingWorkloadDTO;
+import epam.com.practice.trainerservice.handler.exceptions.ResourceNotFoundException;
 import epam.com.practice.trainerservice.service.TrainerWorkloadService;
-import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+
+
 
 @RestController
 @RequestMapping(value = "api/workload")
-@Api(tags = {SwaggerConfiguration.TRAINER_WORKLOAD_TAG})
-@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class TrainersTrainingSessionController {
 
     private final TrainerWorkloadService trainerWorkloadService;
@@ -40,14 +35,23 @@ public class TrainersTrainingSessionController {
             @ApiResponse(responseCode = "403", description = "Forbidden - You don't have permission", content = @Content),
             @ApiResponse(responseCode = "404", description = "Training not found", content = @Content)
     })
-    @PostMapping(value = "/trainers/trainings")
-    public ResponseEntity<String> updateTrainersTrainingWorkload(
+
+    @PostMapping
+    public ResponseEntity<TrainersTrainingWorkloadDTO> updateTrainersTrainingWorkload(
             @RequestBody TrainersTrainingWorkloadDTO trainersTrainingWorkloadRequest) {
 
-        trainerWorkloadService.addTrainerWorkload(
-                trainersTrainingWorkloadRequest);
+        try {
+            trainerWorkloadService.updateTrainerWorkload(
+                    trainersTrainingWorkloadRequest);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(trainersTrainingWorkloadRequest, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(trainersTrainingWorkloadRequest, HttpStatus.OK);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("Training workload created successfully");
     }
 
+    @GetMapping(value = "/summary")
+    public Object getTrainerMonthlySummary() {
+        return trainerWorkloadService.getTrainingMonthlySummary();
+    }
 }
