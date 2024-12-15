f9def950221f502241b5a9849a48770e0fd69563
Dana <Dana.ackerman0914@gmail.com>
2024-12-15 22:36:26 +0500

    Add: Consumer class to receive messages

---
+++ src/main/java/epam/com/practice/trainerservice/jms/ConsumerService.java
@@ -0,0 +1,44 @@
+package epam.com.practice.trainerservice.jms;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import epam.com.practice.trainerservice.dto.TrainingDTO;
+import epam.com.practice.trainerservice.service.TrainerWorkloadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerService {
+
+    private final JmsTemplate jmsTemplate;
+    private final TrainerWorkloadService trainerWorkloadService;
+    private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+
+
+    public ConsumerService(JmsTemplate jmsTemplate,
+                           TrainerWorkloadService trainerWorkloadService,
+                           ObjectMapper objectMapper) {
+
+        this.jmsTemplate = jmsTemplate;
+        this.trainerWorkloadService = trainerWorkloadService;
+        this.objectMapper = objectMapper;
+    }
+
+    @JmsListener(destination = "trainingDTO queue", containerFactory = "jmsListenerContainerFactory")
+    public void receiveMessage(String message) {
+        try {
+            TrainingDTO trainingDTO = objectMapper.readValue(message, TrainingDTO.class);
+            logger.info("Received Message: {}", message);
+
+            trainerWorkloadService.updateTrainerWorkload(trainingDTO);
+            logger.info("Trainer workload updated");
+        } catch (Exception e) {
+            logger.error("Error while receiving message {}", e.getMessage());
+        }
+
+    }
+}
